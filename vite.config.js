2af1bad545e5c8d67af05c87079c536480caa913
Joanna0001 <gkj159@163.com>
2025-10-30 11:41:10 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path';
 
 const CWD = process.cwd();
 
-//配置参考 https://vitejs.dev/config/1
+//配置参考 https://vitejs.dev/config/
 export default defineConfig((mode) => {
   // const { VITE_BASE_URL } = loadEnv(mode, CWD);
   return {
