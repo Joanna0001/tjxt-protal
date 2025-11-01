eedab672df19b30709076d7e0435ff01e5150278
Joanna0001 <gkj159@163.com>
2025-11-01 20:46:11 +0800

    fixed: baseurl

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,77 +1,69 @@
-import axios from 'axios';
-import proxy from '../config/proxy';
-import { ElMessage, ElMessageBox } from 'element-plus';
-import  router  from '../router';
-import {ref} from "vue";
-import {tryRefreshToken} from './refreshToken'
+import axios from "axios";
+import { ElMessageBox } from "element-plus";
+import router from "../router";
+import { tryRefreshToken } from "./refreshToken";
 
-const env = import.meta.env.MODE || 'development';
-const host = env === 'mock' ? 'https://mock.boxuegu.com/mock/3359' : proxy[env].host; // 如果是mock模式 就不配置host 会走本地Mock拦截
 const CODE = {
   LOGIN_TIMEOUT: 1000,
   REQUEST_SUCCESS: 200,
   REQUEST_FOBID: 1001,
 };
 // 登录异常弹窗处理
-let isLogin = true
+let isLogin = true;
 // 刷新标记
 // let refreshing = ref(false)
 
 const instance = axios.create({
-  baseURL:  host, // 'http://172.17.2.134/api-test',
+  baseURL: import.meta.env.VITE_BASE_URL || "/",
   timeout: 1000,
   withCredentials: false,
 });
 
 instance.interceptors.request.use((config) => {
-  const TOKEN = sessionStorage.getItem('token');
+  const TOKEN = sessionStorage.getItem("token");
   config.headers = {
     "Content-Type": "application/json",
-    "authorization": TOKEN
-  }  
-  return config
+    authorization: TOKEN,
+  };
+  return config;
 });
 
 instance.defaults.timeout = 5000;
-async function refreshToken(err){
+async function refreshToken(err) {
   // 尝试刷新token
   let success = await tryRefreshToken();
-  if(success){
+  if (success) {
     // refreshing.value = false;
     return instance(err.config);
   }
   // refreshing.value = false;
-  ElMessageBox.alert(
-    '请先登录！',
-    '未登录或登录超时',
-    {
-      confirmButtonText: '重新登录',
-      callback: () => {
-        router.push('/login')
-      },
-    }
-  )
+  ElMessageBox.alert("请先登录！", "未登录或登录超时", {
+    confirmButtonText: "重新登录",
+    callback: () => {
+      router.push("/login");
+    },
+  });
   return true;
 }
 function alertLoginMessage() {
   isLogin = false;
-  sessionStorage.removeItem('userInfo');
+  sessionStorage.removeItem("userInfo");
   sessionStorage.removeItem("token");
   ElMessageBox.confirm(
-    '您的账号登录超时或在其他机器登录，请重新登录或更换账号登录！',
-    '登录超时',
+    "您的账号登录超时或在其他机器登录，请重新登录或更换账号登录！",
+    "登录超时",
     {
-      confirmButtonText: '重新登录',
-      cancelButtonText: '继续浏览',
-      type: 'warning',
+      confirmButtonText: "重新登录",
+      cancelButtonText: "继续浏览",
+      type: "warning",
     }
-    )
+  )
     .then(() => {
-      router.push('/login')
+      router.push("/login");
     })
     .catch(() => {
-      router.go(0)
-    })
+      router.go(0);
+    });
 }
 // const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 instance.interceptors.response.use(
@@ -90,21 +82,21 @@ instance.interceptors.response.use(
     }
 
     return response.data;
-/*    // 4.业务状态码为其它，返回异常
+    /*    // 4.业务状态码为其它，返回异常
     ElMessage({
       message: response.data.msg,
       type: 'error'
     });
     throw new Error(response.data.msg);*/
   },
-   async (err) => {
-    if(err.response.status === 401 && isLogin){
+  async (err) => {
+    if (err.response.status === 401 && isLogin) {
       // 登录异常或超时，刷新token
       return refreshToken(err);
     }
     // refreshing = false;
     return Promise.reject(err);
-  },
+  }
 );
 
 export default instance;
