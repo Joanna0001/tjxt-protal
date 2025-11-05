08f504fda3f764ffdce64927e6089e6f0a419abc
Joanna0001 <gkj159@163.com>
2025-11-05 23:09:49 +0800

    fixed

--- src/pages/login/components/LoginPass.vue
+++ src/pages/login/components/LoginPass.vue
@@ -48,7 +48,7 @@ const formRef = ref();
 const checked = ref(false)
 // 登录参数效验
 const fromData = reactive({
-  username: "18810966208",
+  username: "rose",
   password: "123456",
   type: 1
 });
--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,8 @@ export default defineConfig({
     port: 18082,
     proxy: {
       '/img-tx': {
-        target: "http://61.153.188.157:10010",
+        target: "http://localhost:10010",
+        // target: "http://61.153.188.157:10010",
         changeOrigin: true,
         rewrite: (path) => {
           return path.replace(/^\/img-tx/, '')
